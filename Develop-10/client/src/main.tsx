0ea63ada92cb7ae7c18c82c521f2e0e678d1d486
Tammy Paquette <tammylpaquette@gmail.com>
2025-04-08 19:50:57 -0400

    Code to be pushed

---
+++ Develop-10/client/src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App.jsx'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+)
